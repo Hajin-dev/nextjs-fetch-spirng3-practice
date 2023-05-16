0c01a744625686f78ed4a50fb9123af0530eb81f
Hajin-dev <hajin1254@protonmail.com>
2023-05-16 16:02:37 +0900

    PUT(데이터 편집) 빼고 기능 모두 추가...?

--- src/app/components/DBTable.tsx
+++ src/app/components/DBTable.tsx
@@ -8,7 +8,33 @@ interface dbInterface {
 interface tableProps{
   data: dbInterface[]
 }
+interface dataProps{
+  data:dbInterface
+}
+
+async function remove(id:string){
+  await fetch('http://localhost:8080/info/'+id,{
+    method:'DELETE',
+    headers:{
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+function EditDB({data}:dataProps){
+  const dropdownId= "menu-"+data.id
+  return(
+    <div>
+      <button
+      className="rounded-full w-16 h-8 bg-blue-400">EDIT</button>
+      <button
+      className="rounded-full w-16 h-8 bg-red-400" onClick={()=>remove(data.id)}>DELETE</button>
+    </div>
+  )
+}
 export default function DBTable({data}:tableProps){
+
   return(
     <table className="table-auto">
       <thead>
@@ -24,7 +50,7 @@ export default function DBTable({data}:tableProps){
         <tr key={d.id}>
           <td className="p-3">{d.name}</td>
           <td className="p-3">{d.email}</td>
-          <td className="p-3 text-center">-</td>
+          <td className="p-3 text-center"><EditDB data={d}/></td>
         </tr>):
         
         <tr>
